78aa6926887aca7f655c52ebae571211847ca340
Ágatha <agathaakempe@gmail.com>
2026-03-07 16:13:05 -0300

    Aula 15 - Testes
    Basicamente a aplicação de um dos 4 testes básicos para programação.

---
+++ aula 15/teste_meumodulo.py
@@ -0,0 +1,14 @@
+# test_meu_modulo
+
+
+
+from meu_modulo import imc
+
+
+
+def test_modulo():
+    assert imc(80,1.80) ==  24.69
+    assert imc(60,1.80) ==  18.52
+    assert imc(50,1.80) ==  15.43
+    assert imc(100,1.80)==  30.86
+
